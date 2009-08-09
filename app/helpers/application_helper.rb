eae304ad9bdb064b50d768cb368918fb342786fb
Michel Belleville <michel.belleville@gmail.com>
2009-08-09 13:02:37 +0200

    Simple layout including a title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title &block
+    if block
+      @title = block
+    elsif @title
+      @title.call
+    end
+  end
 end
