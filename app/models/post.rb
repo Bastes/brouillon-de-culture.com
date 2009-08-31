49441a78003a82166ed5f2a3dd96d0d5bc72ea2a
Michel Belleville <michel.belleville@gmail.com>
2009-09-01 01:05:46 +0200

    Keywords integrated to posts, views updated.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,15 @@ class Post < ActiveRecord::Base
   validates_presence_of :text
 
   has_and_belongs_to_many :keywords
+
+  def keywords_by_word= words
+    words = words.split(/\s*,\s*/) if words.respond_to? :split
+    self.keywords = words.collect { |word|
+      Keyword.find_by_word(word.strip) || Keyword.new(:word => word)
+    }.compact
+  end
+
+  def keywords_by_word
+    self.keywords.collect { |keyword| keyword.word }.join ', '
+  end
 end
