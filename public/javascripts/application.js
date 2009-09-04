1c797b2a98c9dae9be83ed523cc0f91d64ad1586
Michel Belleville <michel.belleville@gmail.com>
2009-09-04 15:23:35 +0200

    Quick crude and dirty ajax-ing of the app.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,40 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $('a').live('click', function() {
+    myself = $(this);
+    var action = false;
+    try { action = myself.attr('href').match(/\/(new|edit|remove)\b/)[1]; }
+    catch(expt) {}
+    if (action) {
+      if (action == 'new') {
+        $('<li/>').load(myself.attr('href') + ' #view', function() {
+          $(this).appendTo(myself.prev('ul'));
+        });
+      }
+      else if (action == 'edit') {
+        myself.closest('li').load(myself.attr('href') + ' #view');
+      }
+      else if (action == 'remove');
+      return false;
+    }
+    return true;
+  });
+
+  $('form').live('submit', function() {
+    myself = $(this);
+    console.log($(this));
+    var action = myself.find('input[name=_method]').val();
+    if (! action) action = myself.attr('method');
+    if (action.match(/put|post|delete/)) {
+      if (action.match(/delete/));
+      else {
+        $.post(myself.attr('action'), myself.serialize(), function(data) {
+          $('#view', data)
+            .find('.nav').remove().end()
+            .appendTo(myself.closest('li').empty());
+        }, 'html');
+      }
+      return false;
+    }
+    return true;
+  });
+});
