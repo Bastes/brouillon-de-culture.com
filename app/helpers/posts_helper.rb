f81a8a9d9607653e8321340e07ee3eb5428ef5e9
Michel Belleville <michel.belleville@gmail.com>
2009-09-16 15:17:58 +0200

    Small but nifty little codify helper method to wrap code without writing nasty-looking html.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  def codify text
+    text.gsub(/\[\[(javascript|css|xml|ruby)\]\]/, '<pre class="code"><code class="\1">')\
+        .gsub(/\[\[\/.*?\]\]/, '</code></pre>')
+  end
 end
