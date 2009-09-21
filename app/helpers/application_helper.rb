8b0bbf5bb42b6bd432fa2bf290ad742046154f1c
Michel Belleville <michel.belleville@gmail.com>
2009-09-21 15:53:23 +0200

    I18n hack for will_paginate.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  include WillPaginate::ViewHelpers 
+
+  def will_paginate_with_i18n(collection, options = {}) 
+    will_paginate_without_i18n(collection, options.merge(
+      :previous_label => I18n.t(:previous, :scope => :will_paginate),
+      :next_label => I18n.t(:next, :scope => :will_paginate))) 
+  end 
+  alias_method_chain :will_paginate, :i18n  
+  
   def title &block
     if block
       @title = block
