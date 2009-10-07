02d223e382c5847593190a43a61ebabdfedea632
Michel Belleville <michel.belleville@gmail.com>
2009-10-07 12:12:28 +0200

    First attempt at testing.

--- test/unit/keyword_test.rb
+++ test/unit/keyword_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class KeywordTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "require word" do
+    k = Keyword.create
+    assert !k.valid?, "Should not be valid without a word"
+
+    k = Keyword.create :word => ''
+    assert !k.valid?, "Should not be valid with an empty word"
+    
+    k = Keyword.create :word => 'word'
+    assert k.valid?
+  end
+
+  test "require an unique word" do
+    base = Keyword.create :word => 'word'
+    assert base.valid?
+
+    k = Keyword.create :word => 'word'
+    assert !k.valid?, "Should not be valid with a duplicate word"
+
+    k = Keyword.create :word => 'another word'
+    assert k.valid?
   end
 end
