f5b8cd8ae06f38225796efe2c8d9d46e11b121c4
Michel Belleville <michel.belleville@gmail.com>
2009-10-09 18:54:12 +0200

    Minimal validations testing for posts.

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "require title" do
+    p = Post.create :text => "blah blah blah"
+    assert !p.valid?, "Should not be valid without a title"
+
+    p = Post.create :title => '', :text => "blah blah blah"
+    assert !p.valid?, "Should not be valid with a blank title"
+
+    p = Post.create :title => 'fake title', :text => "blah blah blah"
+    assert p.valid?
+  end
+  
+  test "unique title" do
+    base = Post.create :title => 'fake title', :text => "blah blah blah"
+    assert base.valid?
+
+    p = Post.create :title => base.title, :text => "blah blah blah"
+    assert !p.valid?, "Should not be valid with duplicate title"
+
+    p = Post.create :title => 'another title', :text => "blah blah blah"
+    assert p.valid?
+  end
+  
+  test "require text" do
+    p = Post.create :title => 'fake title'
+    assert !p.valid?, "Should not be valid without a text"
+
+    p = Post.create :title => 'fake title', :text => ""
+    assert !p.valid?, "Should not be valid with a blank text"
+
+    p = Post.create :title => 'fake title', :text => "blah blah blah"
+    assert p.valid?
   end
 end
