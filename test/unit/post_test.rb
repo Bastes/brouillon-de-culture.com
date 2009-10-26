c2028a27063c52cec7c3d3193c2d5f9b8826b0ba
Michel Belleville <michel.belleville@gmail.com>
2009-10-26 12:35:52 +0100

    Using shoulda to test posts.

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,52 +1,41 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  test "require title" do
-    post = Post.create :text => "blah blah blah"
-    assert !post.valid?, "Should not be valid without a title"
+  should_have_and_belong_to_many :keywords
+  should_validate_uniqueness_of :title
+  should_validate_presence_of :title
+  should_validate_presence_of :text
 
-    post = Post.create :title => '', :text => "blah blah blah"
-    assert !post.valid?, "Should not be valid with a blank title"
+  context "The whole list of posts ordered by creation date" do
+    setup do
+      @posts = Post.by_creation_date.all
+    end
 
-    post = Post.create :title => 'fake title', :text => "blah blah blah"
-    assert post.valid?
-  end
-  
-  test "unique title" do
-    base = Post.create :title => 'fake title', :text => "blah blah blah"
-    assert base.valid?
-
-    post = Post.create :title => base.title, :text => "blah blah blah"
-    assert !post.valid?, "Should not be valid with duplicate title"
-
-    post = Post.create :title => 'another title', :text => "blah blah blah"
-    assert post.valid?
-  end
-  
-  test "require text" do
-    post = Post.create :title => 'fake title'
-    assert !post.valid?, "Should not be valid without a text"
-
-    post = Post.create :title => 'fake title', :text => ""
-    assert !post.valid?, "Should not be valid with a blank text"
-
-    post = Post.create :title => 'fake title', :text => "blah blah blah"
-    assert post.valid?
-  end
-
-  test "order by creation date" do
-    posts = Post.by_creation_date.all
-    created_at = nil
-    posts.each do |post|
-      assert post.created_at <= created_at, "Should be ordered by creation date" if created_at
-      created_at = post.created_at
+    should "be ordered by creation date" do
+      created_at = nil
+      @posts.each do |post|
+        assert post.created_at <= created_at if created_at
+        created_at = post.created_at
+      end
     end
   end
 
-  test "keywords by words" do
-    post = Post.create :keywords_by_word => 'blah, bloh, bleh'
-    assert post.keywords.size == 3
-    assert post.keywords.map(&:word) == ['blah', 'bloh', 'bleh']
-    assert post.keywords_by_word.split(/, /) == ['blah', 'bloh', 'bleh']
+  context "A post with keywords by word" do
+    setup do
+      @post = Post.create :keywords_by_word => 'blah, bloh, bleh'
+    end
+
+    should "show the right amount of keywords" do
+      assert @post.keywords.size == 3
+    end
+
+    should "show these keywords" do
+      assert @post.keywords.map(&:word) == ['blah', 'bloh', 'bleh']
+    end
+
+    should "show these keywords by word" do
+      assert @post.keywords_by_word.split(/, /) == ['blah', 'bloh', 'bleh']
+    end
   end
 end
+
